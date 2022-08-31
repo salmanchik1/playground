d73469cd7b5318c43147e7616ba9061f905dc178
salmanchik1 <salmanov.ramis@gmail.com>
2022-08-31 19:53:05 +0300

    singleton example added

---
+++ singleton.py
@@ -0,0 +1,14 @@
+class Singleton:
+    """An example of singleton design pattern using.
+    """
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super().__new__(cls)
+        return cls.instance
+
+singleton = Singleton()
+new_singleton = Singleton()
+
+print(singleton is new_singleton)
+singleton.single_variable = "Singleton variable"
+print(new_singleton.single_variable)
